3296d682ed6666d28e0eabd04ffbc88c068d1337
cleverjohnson <40ndgc@gmail.com>
2025-10-24 16:14:27 +0200

    Updated CV

--- tokyo/src/components/about/AboutMain.jsx
+++ tokyo/src/components/about/AboutMain.jsx
@@ -31,7 +31,7 @@ const AboutMain = () => {
           {/* End personal info */}
 
           <div className="tokyo_tm_button" data-position="left">
-          <a href="assets/img/cv.pdf" download>
+          <a href="assets/img/Johnson_Clever_Ggalabuzi_Master_CV_with_Complete_Education.pdf" download>
               <span>Download CV</span>
             </a>
           </div>
